8370384965894be37a0dba716272056ae0590f8f
Sul01 <sulrend@gmail.com>
2017-09-29 23:17:26 -0500

    add something

---
+++ ProjectTest/Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private float speed=5;
+    private float hori, verti;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        hori = Input.GetAxis("Horizontal");
+        verti = Input.GetAxis("Vertical");
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(hori, verti, 0);
+        rb.velocity = move * speed;
+    }
+}
